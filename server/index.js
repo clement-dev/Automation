4b3256a40c6d2e6d7ab3b44b62e8192a73d974c3
Ghalnas <frs.grv@gmail.com>
2018-01-25 13:53:43 +0100

    [master] fix : added firebase-admin and load .env

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').load();
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
@@ -6,7 +7,6 @@ const bodyParser = require('body-parser');
 const io = require('socket.io')(server, { pingTimeout: 30000 });
 const PluginController = require('./controllers/PluginController');
 const firebaseService = require('./services/FirebaseService');
-require('dotenv').load();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
